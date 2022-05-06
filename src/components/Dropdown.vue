1f79e2b40e371799bae5811e2bf9d54b8a8abe4b
Francesco Morselli <francesco.morselli@me.com>
2022-05-06 14:41:13 +0200

    Changed some dropdown styles. Now the user can choose to filter out all the radios except the loved ones [now working].

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,14 @@
-<script setup>
+<script setup lang="ts">
+import Loved from "./icons/Loved.vue";
+import List from "./icons/List.vue";
+import Caret from "./icons/Caret.vue";
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+</script>
+
+<script lang="ts">
+export default {
+  name: "Dropdown",
+};
 </script>
 
 <template>
@@ -9,17 +18,8 @@ import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
         <MenuButton
           class="inline-flex w-full border-gray-500 border justify-center rounded-3xl px-4 py-2 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
-          <div class="text-sm">All</div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-3 ml-3"
-            viewBox="0 0 320 512"
-          >
-            <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-            <path
-              d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"
-            />
-          </svg>
+          <div class="text-sm">Filter</div>
+          <Caret class="w-3 ml-3"></Caret>
         </MenuButton>
       </div>
 
@@ -32,62 +32,27 @@ import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
         leave-to-class="transform scale-95 opacity-0"
       >
         <MenuItems
-          class="absolute right-3 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          class="absolute right-3 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-3xl bg-white drop-shadow-md ring-1 ring-black ring-opacity-5 focus:outline-none"
         >
           <div class="px-1 py-1">
             <MenuItem v-slot="{ active }">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="{
+                  'bg-gray-100': active,
+                }"
+                class="group flex w-full items-center rounded-3xl px-2 py-2 text-sm text-gray-900"
               >
-                Edit
+                <Loved class="ml-1 mr-3 w-5"></Loved>Preferred
               </button>
             </MenuItem>
             <MenuItem v-slot="{ active }">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="{
+                  'bg-gray-100': active,
+                }"
+                class="group flex w-full items-center rounded-3xl px-2 py-2 text-sm text-gray-900"
               >
-                Duplicate
-              </button>
-            </MenuItem>
-          </div>
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-                Archive
-              </button>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-                Move
-              </button>
-            </MenuItem>
-          </div>
-
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-                Delete
+                <List class="ml-2 mr-4 w-5"></List>All
               </button>
             </MenuItem>
           </div>
