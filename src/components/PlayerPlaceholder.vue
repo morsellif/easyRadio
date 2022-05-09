ee00a530bf3bf9c8f9e9fd83d9b32a8dde02ddde
Francesco Morselli <francesco.morselli@me.com>
2022-05-09 17:20:05 +0200

    Run Prettier.

--- src/components/PlayerPlaceholder.vue
+++ src/components/PlayerPlaceholder.vue
@@ -1,9 +1,9 @@
 <template>
-  <div
-    class="bg-white border border-gray-200 rounded-3xl drop-shadow-md mb-4 p-4"
-  >
-    <div class="font-bold text-3xl text-center">
-      Start by selecting a radio.
-    </div>
-  </div>
+	<div
+		class="bg-white border border-gray-200 rounded-3xl drop-shadow-md mb-4 p-4"
+	>
+		<div class="font-bold text-3xl text-center">
+			Start by selecting a radio.
+		</div>
+	</div>
 </template>
