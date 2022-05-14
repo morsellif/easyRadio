4ec1d3fe1b4b340239a7170d5e78ac1c4bebb956
Francesco Morselli <francesco.morselli@me.com>
2022-05-14 15:03:57 +0200

    Redesigned logo.

--- src/components/HeadingComponent.vue
+++ src/components/HeadingComponent.vue
@@ -1,20 +1,16 @@
 <script setup lang="ts">
-import Radio from './icons/RadioIcon.vue';
+import TowerBroadcastIcon from './icons/TowerBroadcastIcon.vue';
 </script>
 
 <template>
 	<div
 		class="bg-white border border-gray-200 rounded-3xl drop-shadow-md mb-4 mt-4 p-4"
 	>
-		<div class="flex flex-column bg-white border-1">
-			<div class="pr-4 align-middle">
-				<Radio class="fill-black w-12"></Radio>
+		<div class="flex flex-column bg-white border-1 justify-center">
+			<div class="align-middle">
+				<TowerBroadcastIcon class="fill-black w-12"></TowerBroadcastIcon>
 			</div>
-			<div
-				class="inline-block align-middle grow self-center font-bold text-3xl"
-			>
-				easyRadio
-			</div>
+			<div class="flex self-end text-2xl">easyRadio</div>
 		</div>
 	</div>
 </template>
