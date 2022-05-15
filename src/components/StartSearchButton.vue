44161bc9fc2126b81a7c1e9673391d7a08de23f2
Francesco Morselli <francesco.morselli@me.com>
2022-05-15 15:34:48 +0200

    Removed background opacity on hover.

--- src/components/StartSearchButton.vue
+++ src/components/StartSearchButton.vue
@@ -5,7 +5,7 @@ import MagnifyingGlassIcon from './icons/MagnifyingGlassIcon.vue';
 <template>
 	<div class="flex items-center">
 		<button
-			class="hover:bg-slate-100 rounded-full transition ease-in-out delay-150 p-1 inline-flex w-full border-gray-500 border justify-center px-2 py-2 mr-1 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+			class="hover:bg-slate-100 rounded-full transition ease-in-out delay-150 p-1 inline-flex w-full border-gray-500 border justify-center px-2 py-2 mr-1 text-sm font-medium text-black focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
 		>
 			<MagnifyingGlassIcon
 				class="h-5 w-5 fill-black text-center"
