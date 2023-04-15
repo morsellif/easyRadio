92cf7decbd8f9d88dba8c5ad0c22764a4f49741f
Francesco Morselli <francesco.morselli@me.com>
2023-04-15 14:03:00 +0200

    Basic configuration for Nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+export default defineNuxtConfig({
+	modules: [
+		'@pinia/nuxt',
+		// '@pinia-plugin-persistedstate/nuxt',
+		'@vueuse/nuxt',
+		'@nuxt/devtools',
+		'@nuxtjs/i18n',
+	],
+	/* piniaPersistedstate: {
+		storage: 'localStorage',
+	}, */
+	build: {},
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	i18n: {
+		locales: [
+			{
+				code: 'en',
+				file: 'en-US.js',
+			},
+			{
+				code: 'it',
+				file: 'it-IT.js',
+			},
+		],
+		strategy: 'no_prefix',
+		lazy: true,
+		langDir: 'lang/',
+		defaultLocale: 'en',
+	},
+	typescript: {},
+	css: ['~/assets/css/main.css'],
+	srcDir: './src/',
+	app: {
+		head: {
+			title: 'laSpesa',
+			meta: [{ name: 'description', content: '' }],
+		},
+	},
+	ssr: true,
+});
