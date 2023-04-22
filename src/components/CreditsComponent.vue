595c422d5a9c8bd3891dea02a6268283d12cfd85
Francesco Morselli <francesco.morselli@me.com>
2023-04-22 16:24:41 +0200

    Removed <li> tags with <div>

--- src/components/CreditsComponent.vue
+++ src/components/CreditsComponent.vue
@@ -4,10 +4,10 @@ import GitHubIcon from './icons/GitHubIcon.vue';
 
 <template>
 	<a href="https://github.com/morsellif/easy-radio" target="_blank">
-		<li
+		<div
 			class="text-center m-3 text-lg font-semibold underline underline-offset-4"
 		>
 			Contribute on<GitHubIcon class="w-7 inline-block ml-3"></GitHubIcon>
-		</li>
+		</div>
 	</a>
 </template>
