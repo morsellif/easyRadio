6e139f0135bb68b6f4974717a70a849980d72753
Francesco Morselli <francesco.morselli@me.com>
2023-04-22 15:23:01 +0200

    Standardized height of player placeholder with the player component

--- src/components/PlayerPlaceholder.vue
+++ src/components/PlayerPlaceholder.vue
@@ -1,7 +1,9 @@
+<script setup lang="ts"></script>
+
 <template>
 	<div
-		class="bg-white border border-gray-200 rounded-3xl drop-shadow-sm mb-4 p-4"
+		class="bg-white border border-gray-200 rounded-3xl drop-shadow-sm mb-4 p-4 h-[90px] flex items-center content-center"
 	>
-		<div class="text-xl text-center">Start by selecting a radio.</div>
+		<div class="text-xl text-center grow">Start by selecting a radio.</div>
 	</div>
 </template>
