e02e210f80aa30c994665a993568e76eb5ad37fa
Francesco Morselli <francesco.morselli@me.com>
2023-04-22 15:08:08 +0200

    Better spacing management, player order changed for better accessibility

--- src/components/HeadingComponent.vue
+++ src/components/HeadingComponent.vue
@@ -3,9 +3,7 @@ import TowerBroadcastIcon from './icons/TowerBroadcastIcon.vue';
 </script>
 
 <template>
-	<div
-		class="bg-white border border-gray-200 rounded-3xl drop-shadow-sm mb-3 mt-3 p-1"
-	>
+	<div class="bg-white border border-gray-200 rounded-3xl drop-shadow-sm p-1">
 		<div class="flex flex-column bg-white border-1 justify-center rounded-3xl">
 			<div class="align-middle">
 				<TowerBroadcastIcon class="fill-black w-5"></TowerBroadcastIcon>
